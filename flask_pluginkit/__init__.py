7747585f935a3f93085d52b8a6286372fddf01d2
Mr.tao <staugur@saintic.com>
2019-04-09 16:39:25 +0800

    chore: update package all

--- flask_pluginkit/__init__.py
+++ flask_pluginkit/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from .exceptions import PluginError, TarError, ZipError, InstallError, CSSLoadError, DCPError, VersionError, DFPError, NotCallableError
-from .flask_pluginkit import PluginManager, push_dcp
+from .flask_pluginkit import PluginManager, push_dcp, emit_config
 from .installer import PluginInstaller
 from .web import blueprint
 from .fixflask import Flask
@@ -11,6 +11,6 @@ __author__ = "staugur <staugur@saintic.com>"
 
 __version__ = "2.3.2"
 
-__all__ = ["Flask", "PluginManager", "PluginInstaller", "blueprint", "push_dcp",
+__all__ = ["Flask", "PluginManager", "PluginInstaller", "blueprint", "push_dcp", "emit_config",
            "PluginError", "TarError", "ZipError", "InstallError", "CSSLoadError", "DCPError", "VersionError", "DFPError", "NotCallableError",
            "BaseStorage", "LocalStorage", "RedisStorage", "PY2", "string_types", "isValidSemver", "sortedSemver"]
