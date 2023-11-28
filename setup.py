6bb24c00067db66c9f0424c7233d26e973e72dd0
Martin Gross <martin@pc-coholic.de>
2023-11-28 20:22:41 +0100

    Limit to Flask<3.0.0

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.3.*,!=3.5.*",
-    install_requires=["Flask>=0.12", "semver>=2.10.0,<3.0.0"],
+    install_requires=["Flask>=0.12,<3.0.0", "semver>=2.10.0,<3.0.0"],
     cmdclass={
         "publish": PublishCommand,
     },
